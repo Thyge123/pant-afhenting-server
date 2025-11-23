9bb992235e7ebd1d61825b4eb0f1fceae48273a1
Thyge123 <95471753+Thyge123@users.noreply.github.com>
2025-11-23 22:24:20 +0100

    define all database associations

--- app/models/index.js
+++ app/models/index.js
@@ -23,4 +23,78 @@ db.categories = require("./category.model.js")(sequelize, Sequelize);
 db.products = require("./product.model.js")(sequelize, Sequelize);
 db.pickUps = require("./pickUp.model.js")(sequelize, Sequelize);
 
+// One user can have many activities
+db.users.hasMany(db.activities, {
+  foreignKey: "userId",
+  as: "activities",
+});
+db.activities.belongsTo(db.users, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+// One activity can have many activity items
+db.activities.hasMany(db.activityItems, {
+  foreignKey: "activityId",
+  as: "activityItems",
+});
+db.activityItems.belongsTo(db.activities, {
+  foreignKey: "activityId",
+  as: "activity",
+});
+
+// One activity item belongs to one product
+db.activityItems.belongsTo(db.products, {
+  foreignKey: "productId",
+  as: "product",
+});
+
+// One category can have many products
+db.categories.hasMany(db.products, {
+  foreignKey: "categoryId",
+  as: "products",
+});
+db.products.belongsTo(db.categories, {
+  foreignKey: "categoryId",
+  as: "category",
+});
+
+// One activity belongs to one status
+db.activities.belongsTo(db.activityStatus, {
+  foreignKey: "StatusId",
+  as: "activityStatus",
+});
+
+// One activity can have one report
+db.activities.hasOne(db.reports, {
+  foreignKey: "activityId",
+  as: "report",
+});
+db.reports.belongsTo(db.activities, {
+  foreignKey: "activityId",
+  as: "activity",
+});
+
+// One report belongs to one report reason
+db.reports.belongsTo(db.reportReasons, {
+  foreignKey: "reportReasonId",
+  as: "reportReason",
+});
+
+// One activity can have one pick up
+db.activities.hasOne(db.pickUps, {
+  foreignKey: "activityId",
+  as: "pickUp",
+});
+db.pickUps.belongsTo(db.activities, {
+  foreignKey: "activityId",
+  as: "activity",
+});
+
+// One pick up belongs to one user
+db.pickUps.belongsTo(db.users, {
+  foreignKey: "userId",
+  as: "user",
+});
+
 module.exports = db;
